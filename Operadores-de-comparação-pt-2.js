857ddc46cad1e9ed408e5434c104cb93419088ed
walterdyna <alexdyna@gmail.com>
2024-12-11 18:14:49 -0300

    vai dar

---
+++ Operadores-de-comparação-pt-2.js
@@ -0,0 +1,21 @@
+/*Operadores de comparação pt.2
+
+
+>  -> Maior
+<  -> Menor
+
+>= -> Maior ou igual
+<= -> Menor ou igual
+
+Eles são usados para comparar valores e retornam um valor booleano (true ou false). Vamos ver como aplicá-los:
+*/
+
+
+const primeiroNumero = 30;
+const segundoNumero = 20;
+
+console.log (primeiroNumero > segundoNumero); // true
+console.log (primeiroNumero < segundoNumero); // false
+
+console.log (primeiroNumero >= segundoNumero); // true
+console.log (primeiroNumero <= segundoNumero); // false
